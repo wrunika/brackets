77f78b5fe0739119ab4fa817f895c0bc378d56c3
wrunika <2805nika@gmail.com>
2022-08-12 20:59:26 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let bracketsConfigFlat = bracketsConfig.flat();
+  let bracketsPairs = {};
+  let stack = [];  
+  
+  for (let i = 0; i < bracketsConfigFlat.length - 1; i = i + 2) {
+    bracketsPairs[bracketsConfigFlat[i + 1]] = bracketsConfigFlat[i];
+  };
+  
+  let openBrackets = [];
+  for (let index = 0; index < bracketsConfigFlat.length; index++) {
+    if (index % 2 == 0) {openBrackets.push(bracketsConfigFlat[index])};
+  };
+  
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+  
+    if (stack.length != 0 && bracketsPairs[currentSymbol] === topElement ) {
+      stack.pop();
+    } else {
+      stack.push(currentSymbol); 
+    }
+  }
+  
+  return stack.length === 0;
 }
